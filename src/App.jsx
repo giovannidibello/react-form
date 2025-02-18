534fbb43f5e707b2b0ba9bb3cf49fd6e8c4a8409
Giovanni <dibellogio@gmail.com>
2025-02-18 15:00:56 +0100

    add articles list Milistone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 
+// array di articoli
+const articoli = ["Laptop", "Smartphone", "Auricolari", "Zaino", "Tastiera meccanica", "Monitor 4K", "Mouse wireless", "Hard disk esterno"];
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <ul className="lista">
+        {articoli.map(articolo => (<li>{articolo}</li>))}
+      </ul >
     </>
   )
 }
